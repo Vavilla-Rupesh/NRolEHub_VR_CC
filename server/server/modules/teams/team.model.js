378d1e9da975fccc314edd2931fdedebaf611497
Rupesh Vavilla <137624310+Vavilla-Rupesh@users.noreply.github.com>
2025-08-21 21:41:24 +0530

    Update team.model.js

--- server/server/modules/teams/team.model.js
+++ server/server/modules/teams/team.model.js
@@ -1,3 +1,49 @@
+// const { DataTypes } = require('sequelize');
+// const { sequelize } = require('../../config/dataBase');
+// const User = require('../auth/auth.model');
+
+// const Team = sequelize.define('Team', {
+//     id: {
+//         type: DataTypes.INTEGER,
+//         primaryKey: true,
+//         autoIncrement: true
+//     },
+//     name: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//         unique: true,
+//         validate: {
+//             notEmpty: true,
+//             len: [1, 100]
+//         }
+//     },
+//     event_id: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false
+//     },
+//     subevent_id: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false
+//     },
+//     leader_id: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false,
+//         references: {
+//             model: User,
+//             key: 'id'
+//         }
+//     },
+//     created_at: {
+//         type: DataTypes.DATE,
+//         defaultValue: DataTypes.NOW
+//     }
+// }, {
+//     tableName: 'Teams',
+//     timestamps: false
+// });
+
+// module.exports = Team;
+
 const { DataTypes } = require('sequelize');
 const { sequelize } = require('../../config/dataBase');
 const User = require('../auth/auth.model');
@@ -11,7 +57,6 @@ const Team = sequelize.define('Team', {
     name: {
         type: DataTypes.STRING,
         allowNull: false,
-        unique: true,
         validate: {
             notEmpty: true,
             len: [1, 100]
